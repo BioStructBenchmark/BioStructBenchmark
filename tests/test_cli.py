de3f2773a56e811f6614224619eaa70f1089e698
DeltaDesign <deltakapp@gmail.com>
2025-07-15 13:51:49 -0700

    add file format autodetection

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 import pytest
 from pathlib import Path
-from biostructbenchmark.cli import file_type, validate_file_path
+from biostructbenchmark.cli import validate_file_path
 
 
 def test_validate_file_path():
@@ -16,22 +16,3 @@ def test_invalid_file():
 def test_empty_file():
     with pytest.raises(ValueError):
         assert validate_file_path("./tests/data/empty.cif")
-
-
-def test_file_type_pdb():
-    assert file_type("Pdb") == "PDB"
-
-
-def test_file_type_mmcif():
-    assert file_type("Cif") == "MMCIF"
-    assert file_type("mmciF") == "MMCIF"
-
-
-def test_file_type_other():
-    with pytest.raises(ValueError):
-        file_type("other")
-
-
-def test_file_type_blank():
-    with pytest.raises(ValueError):
-        file_type("")
